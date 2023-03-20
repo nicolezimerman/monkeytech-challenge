17020f396559868e082f2d123cd9524c828c4eea
nzimerman <nicole.zimerman@gmail.com>
2023-03-20 19:24:55 +0200

    first commit. Create project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header>
+        <h2>The Jungle™ FastRider Service </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
